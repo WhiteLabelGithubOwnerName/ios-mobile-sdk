801bcc1e5d476c1d958017f9c1603670d270f751
wallee-deployment-user <technical-systems@wallee.com>
2023-10-03 13:31:25 +0000

    Release version: 1.2.0-PostFinanceFinal

---
+++ PostFinanceSdk.xcframework/ios-arm64/PostFinanceSdk.framework/Headers/PostFinanceSdk.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for PostFinanceSdk.
+FOUNDATION_EXPORT double PostFinanceSdkVersionNumber;
+
+//! Project version string for PostFinanceSdk.
+FOUNDATION_EXPORT const unsigned char PostFinanceSdkVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <PostFinanceSdk/PublicHeader.h>
